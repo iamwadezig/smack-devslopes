31a0e5a272f40f8b42ad7455d2cb2a20dbcc2205
Ario Nugroho <gamerzdope@gmail.com>
2019-08-24 20:46:47 +0700

    added JSON parsing with swiftyJSON and the traditional way to register user

--- smack@devslopes/Services/AuthService.swift
+++ smack@devslopes/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     
@@ -46,17 +47,13 @@ class AuthService {
     func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
         
         let lowerCaseEmail = email.lowercased()
-        //create object name header that contain key and value required
-        let header = [
-            "Content-Type" : "application/json; charset=utf-8"
-        ]
         
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
             
             if response.result.error == nil {
                 completion(true)
@@ -67,6 +64,55 @@ class AuthService {
         }
         
     }
+    
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        //create object name header that contain key and value required
+
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+//                //the basic of JSON Parsing without SwiftyJSON
+//                if let json = response.result.value as? Dictionary<String, Any> {
+//                    if let email = json["user"] as? String {
+//                        self.userEmail = email
+//                    }
+//                    if let token = json["token"] as? String {
+//                        self.authToken = token
+//                    }
+//                }
+                //using SwiftyJSON                
+                guard let data = response.data else { return }
+                
+                do {
+                    let json = try JSON(data: data)
+                    self.userEmail = json["user"].stringValue
+                    self.authToken = json["token"].stringValue
+                } catch {
+                    print(error)
+                }
+                
+                
+
+                self.isLoggedIn = true
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+        
+    }
+    
+    
     
     
 }
--- smack@devslopes/Utilities/Constants.swift
+++ smack@devslopes/Utilities/Constants.swift
@@ -13,11 +13,22 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 //URL Constant
 let BASE_URL = "https://smackchitie.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+
 //Segues
 let TO_LOGIN = "loginSegue"
 let TO_CREATE_ACCOUNT = "createAccountSegue"
 let TO_CHANNEL = "unwindToChannel"
+
 //User Defaults
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+//Header
+//create object name header that contain key and value required
+let HEADER = [
+    "Content-Type" : "application/json; charset=utf-8"
+]
+
+
