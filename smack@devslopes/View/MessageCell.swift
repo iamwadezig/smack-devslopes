c04c4a5d7668e9d046912d1a0743f55939f94a15
Ario Nugroho <gamerzdope@gmail.com>
2019-09-03 15:11:59 +0700

    added timestamp

--- smack@devslopes/View/MessageCell.swift
+++ smack@devslopes/View/MessageCell.swift
@@ -27,8 +27,26 @@ class MessageCell: UITableViewCell {
         nameLabel.text = message.userName
         avatarLabel.image = UIImage(named: message.userAvatar)
         avatarLabel.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
-        timeStampLabel.text = MessageService.instance.returnTimeStamp(components: message.timeStamp)
         
-    }
+        //timestamp parse from JSON we got
+        //2017-07-13T21:49:25.590Z
 
+        //endIndex from end ie. "Z" then -5 character ie ".590Z"
+        guard var isoDate = message.timeStamp else {return}
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        //so we get 2017-07-13T21:49:25
+        isoDate = String(isoDate[..<end])
+        //
+        let isoFormatter = ISO8601DateFormatter()
+        //change string into ISO8601Date format, transform from 2017-07-13T21:49:25.590Z to 2017-07-13T21:49:25Z
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        //use another dateformatter so its more readable 
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLabel.text = finalDate
+        }
+    }
 }
